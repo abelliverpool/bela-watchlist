62f8accbe0426102902fc96439533146f68a6826
abelliverpool <145291818+abelliverpool@users.noreply.github.com>
2024-03-09 22:10:29 -0800

    img Update script.js

--- script.js
+++ script.js
@@ -40,13 +40,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 (item.genres && item.genres.some(genre => genre.toLowerCase().includes(searchTerm))))) {
                 const itemElement = document.createElement("div");
                 itemElement.classList.add("watchlist-item");
-                
+                itemElement.style.backgroundImage = item.image ? `url('${item.image}')` : ''; 
                 itemElement.innerHTML = `
-                   <div class="watchlist-item" style="background-image: url('${item.image}');">
                     <h3 class="editable" data-property="title">${item.title}</h3>
                     <p>Type: ${item.type}</p>
                     ${item.type !== "movie" ? `<p>Episodes: <span class="editable" data-property="episodes">${item.episodes}</span></p>` : ''}
                     ${item.type === "series" || item.type === "anime" || item.type === "kdrama" ? `<p>Seasons: <span class="editable" data-property="seasons">${item.seasons}</span></p>` : ''}                    
+                    ${item.image ? `<img src="${item.image}" alt="${item.title}">` : ''}
                     <p>Status: <span class="editable" data-property="status">${item.status}</span></p>
                     <p>Genres: <span class="editable" data-property="genres">${item.genres ? item.genres.join(", ") : 'N/A'}</span></p>
                     <p>Release Date: <span class="editable" data-property="releaseDate">${item.releaseDate}</span></p>
